3f9e793524f527a853bcf273e8c439f2c0759884
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-03-21 21:32:25 +0100

    Updates

---
+++ precache-manifest.6d6c1fddec4e173978be326daa9fa4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f69500b07502fe3c5bdfbb9525085d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4533173a3a426d4259e7",
+    "url": "/robofriends/static/css/2.f8399ec5.chunk.css"
+  },
+  {
+    "revision": "bab0d0f8fbb173e8dd9a",
+    "url": "/robofriends/static/css/main.98284fdd.chunk.css"
+  },
+  {
+    "revision": "4533173a3a426d4259e7",
+    "url": "/robofriends/static/js/2.74dba0d3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.74dba0d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bab0d0f8fbb173e8dd9a",
+    "url": "/robofriends/static/js/main.09dd9305.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
