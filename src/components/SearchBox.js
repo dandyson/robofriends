2b0f7f4c22749dadb7e3eef2ea5d9e5c2b0a08a6
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-03-23 19:30:53 +0100

    Added redux and updated react-scripts

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const SearchBox = ({ searchfield, searchChange}) => {
-    return(
-        <div className = 'pa2'>
-            <input classname='pa3'
-            type="search" 
-            placeholder="Search Robots..." 
-            onChange={searchChange}
-            />
-        </div>
-    )
-}
+const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3"
+        type="search"
+        placeholder="Search Robots..."
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Cardlist from "../components/Cardlist";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
 import { ButtonToolbar } from "react-bootstrap";
+
+import { setSearchField } from "../actions.js";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 
 class App extends Component {
   constructor() {
@@ -20,22 +35,18 @@ class App extends Component {
       .then(users => this.setState({ robots: users }));
   }
 
-  onSearchChange = event => {
-    this.setState({ searchfield: event.target.value });
-  };
-
   render() {
-    const { robots, searchfield } = this.state;
-
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     return !robots.length ? (
       <h1>Loading</h1>
     ) : (
       <div className="tc">
         <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <Cardlist robots={filteredRobots} />
         </Scroll>
@@ -44,4 +55,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
